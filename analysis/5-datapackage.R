8ed7c1c1056ac899d035e307b484de979151df78
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-01-16 15:27:02 +0100

    Update 5-datapackage.R

--- analysis/5-datapackage.R
+++ analysis/5-datapackage.R
@@ -1,50 +1,119 @@
 library(GeoLocatoR)
 
-## Publish Data Pacakge
-# See https://raphaelnussbaumer.com/GeoLocatoR/articles/create-from-geopressuretemplate.html
+## Publish Data Package
+# Introduction: hhttps://raphaelnussbaumer.com/GeoPressureManual/geolocator-intro.html
+# Detailed instruction: https://raphaelnussbaumer.com/GeoPressureManual/geolocator-create.html
 
 # Create the datapackage
 pkg <- create_gldp_geopressuretemplate(".")
 
-# Modify metadata
-pkg$title <- "GeoLocator Data Package: South African Woodland Kingfisher"
-# pkg$embargo <- "2030-01-01"
-pkg$keywords <- c("Woodland Kingfisher", "intra-african", "multi-sensor geolocator")
-# Add DOI of the datapackage if already available or reserve it https://help.zenodo.org/docs/deposit/describe-records/reserve-doi/#reserve-doi
-pkg$id <- "https://doi.org/10.5281/zenodo.13829929"
-# Provide the recommended citation for the package
-pkg$citation <- "Nussbaumer, R., Rime, Y., & Osinubi, S. T. (2024). GeoLocator Data Package: South African Woodland Kingfisher (1.2) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.13829929"
-# Funding sources
-pkg$grants <- c("Swiss Ornithological Intitute")
-# Identifiers of resources related to the package (e.g. papers, project pages, derived datasets, APIs, etc.).
-pkg$relatedIdentifiers <- list(
+#################
+# Create Metadata
+pkg$title <- "GeoLocator Data Package: {Species} in {country}"
+
+# Contributors/creators:
+# Default is to take the GeoPressureTemplate authors, but it is common that
+# additional co-authors should be added for the datapackage
+pkg$contributors <- list( # required
   list(
-    relationType="IsPartOf",
-    relatedIdentifier="10.5281/zenodo.11207081",
-    relatedIdentifierType="DOI"
-  ),
-  list(
-    relationType="IsSupplementTo",
-    relatedIdentifier="",
-    relatedIdentifierType="DOI"
+    title = "Raphaël Nussbaumer",
+    roles = c("ContactPerson", "DataCurator", "ProjectLeader"),
+    email = "raphael.nussbaumer@vogelwarte.ch",
+    path = "https://orcid.org/0000-0002-8185-1020",
+    organization = "Swiss Ornithological Institute"
   )
 )
-# List of references related to the package
-pkg$references <- NULL
-# Add reference_location. Computed automatically when adding data
-# pkg$reference_location <- list(lat = 45.211,  lon = 34.25)
 
-# Add data
-pkg <- pkg %>% add_gldp_geopressuretemplate(".")
+# There are no embargo by default (1970-01-01)
+pkg$embargo <- "2030-01-01"
+
+# Licences
+# Code licenses are often not the same as data licences.
+pkg$licenses = list(list(
+  name = "CC-BY-4.0",
+  title = "Creative Commons Attribution 4.0",
+  path = "https://creativecommons.org/licenses/by/4.0/"
+))
+
+# Review the description, often, you will want to be more talkative here.
+# pkg$description
+
+# Add keywords
+pkg$keywords <- c("Woodland Kingfisher", "intra-african", "multi-sensor geolocator")
+
+# Funding sources
+pkg$grants <- c("Swiss Ornithological Intitute")
+
+# Related Identifiers
+# e.g. papers, project pages, derived datasets, etc.
+pkg$relatedIdentifiers <- list(
+  list(
+    relationType = "IsPartOf",
+    relatedIdentifier = "10.5281/zenodo.11207081",
+    relatedIdentifierType = "DOI"
+  )
+)
+
 # print(pkg)
 
+
+
+
+#################
+# Add data
+pkg <- pkg %>% add_gldp_geopressuretemplate()
+
+# Add tags and observations
+tags(pkg) <- read_csv("data/tags.csv")
+observations(pkg) <- read_csv( "data/observations.csv")
+
 # Check package
-check_gldp(pkg)
+# plot(pkg)
+validate_gldp(pkg)
 
+
+
+
+#################
 # Write datapackage
-dir.create("data/datapackage", showWarnings = FALSE)
-write_package(pkg, "data/datapackage/")
 
-# Upload on Zenodo
+## Option 1: Manual
 # https://zenodo.org/uploads/new
+pkg$id <- "https://doi.org/10.5281/zenodo.{ZENODO_ID - 1}"
+# e.g. "10.5281/zenodo.14620590" for a DOI reserved as 10.5281/zenodo.14620591
+# Update the bibliographic citation with this new DOI
+pkg <- pkg %>% update_gldp_bibliographic_citation()
+
+dir.create("data/datapackage", showWarnings = FALSE)
+frictionless::write_package(pkg, "data/datapackage/")
+
 # Use the information in datapackage.json to fill the zenodo form.
+
+## Option 2: API
+# Create token and Zenodo manager
+# https://zenodo.org/account/settings/applications/tokens/new/
+keyring::key_set_with_value("ZENODO_PAT", password = "{your_zenodo_token}")
+zenodo <- ZenodoManager$new(token = keyring::key_get(service = "ZENODO_PAT"))
+
+# Create a zenodo from data package
+z <- gldp2zenodoRecord(pkg)
+
+z <- zenodo$depositRecord(z, reserveDOI = TRUE, publish = FALSE)
+
+pkg$id <- paste0("https://doi.org/", z$getConceptDOI())
+pkg <- pkg %>%
+  update_gldp()
+
+write_package(pkg, directory = "data/datapackage/")
+for (f in list.files(pkg$version)) {
+  zenodo$uploadFile(file.path(pkg$version, f), z)
+}
+
+
+#################
+# Update zenodo
+z_updated <- zenodo$getDepositionByConceptDOI(z$getConceptDOI())
+pkg <- zenodoRecord2gldp(z_updated, pkg)
+
+
+## Make sure to submit to Zenodo community: https://zenodo.org/communities/geolocator-dp/
